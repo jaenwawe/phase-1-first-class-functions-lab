1395a3698f0c755c9eaa88515f66a34c8f3ded56
jaenwawe <nwawe.jae@gmail.com>
2021-08-16 22:30:47 -0500

    commit tree 081a40cab80e0fc2b505f0823436b3a55fbfa23d

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+        const seperate = array.slice(0,2);
+        return seperate;
+    }
+
+const returnLastTwoDrivers = (array) => {
+    const seperate = array.slice(-2);
+    return seperate;
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+
+function  createFareMultiplier(multiple){
+    return ()=> 4 * multiple;
+}
+
+
+
+ function  createFareMultiplier(){
+     return function () {
+    const fareMultiplier = createFareMultiplier(2);
+ }
+}
